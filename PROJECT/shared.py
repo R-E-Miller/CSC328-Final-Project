f6ffd9b2265420fa113dd91819d9fe418e1b2e66
el-Swan <e.swan12345@gmail.com>
2023-11-29 15:47:03 -0500

    Added a shared object library
    Contains functions that can be used by both server and client

---
+++ PROJECT/shared.py
@@ -0,0 +1,17 @@
+import socket
+import json
+
+def send_message(connection, msg, nick):
+    myData = {'msg': msg, 'nick': nick}
+    myMsg = json.dumps(myData) 
+    myMsg = myMsg.encode()
+    connection.sendall(myData)
+
+def read_message(connection):
+    #TODO: THIS HAS TO CHANGE TO ALLOW LARGER MESSAGES
+    receivedMsg = connection.recv(1024)
+    receivedMsg = receivedMsg.decode()
+    msg = receivedMsg['msg']
+    nick = receivedMsg['nick']
+    return (nick, msg)
+
