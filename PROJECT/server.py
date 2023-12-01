4f1cc28e57fae1040fd3a5e326fb412c42fa3019
el-Swan <e.swan12345@gmail.com>
2023-12-01 17:43:31 -0500

    Added select so that we only read from file descriptors that need reading
    Added use of select.

--- PROJECT/server.py
+++ PROJECT/server.py
@@ -4,14 +4,11 @@ import select
 import sys
 import json
 import shared as sh
+import time
 
 def check_nick(s, storedname):
     #read nickname from the client
     bannedname = ["SERVER"]
-    world = "HELLO"
-    name = "SERVER"
-    proto = ''
-    sh.send_message(s,world, name, proto)
     message = ""
     while message != "READY":
         nic,msg,proto = sh.read_message(s)
@@ -25,11 +22,16 @@ def check_nick(s, storedname):
     #reply back with either approved name or already taken
     #
 
+def send_hello(s):
+    world = "HELLO"
+    name = "SERVER"
+    proto = ''
+    sh.send_message(s,world, name, proto)
 
 def main():
     nickname = []
-    port = int(sys.argv[1])
-    host = sys.argv[2]
+    port = int(sys.argv[2])
+    host = sys.argv[1]
     if not (10000 <= port <= 65535):
         print("Port number not available")
         sys.exit(-1)
@@ -40,12 +42,29 @@ def main():
             s.bind((host, port))
             s.listen(10)
             print("\n Server listening")
+            myConnectionsSetup = [s]
+            myConnections, _, _ = select.select(myConnectionsSetup, [], []) 
+
             while True:
-                connection, _ = s.accept()
-                with connection:
-                    print("Got connection")
-                    check_nick(connection, nickname )
-                    print("SERVER TIME")
+                myConnections, _,_ = select.select(myConnectionsSetup, [], [], 0)
+                if myConnections == []:
+                    #Feel free to comment this out I feel it gives a pretty good understanding of what is actually happening
+                    print("Never blocked")
+                    time.sleep(1)
+                else:
+                    for readySock in myConnections:
+                        #This only will go off if we need to accept a connection and say hello
+                        if readySock == s:
+                            connection, addr = s.accept()
+                            print(f"Got connection from {addr}")
+                            myConnectionsSetup.append(connection)
+                            send_hello(connection)
+                        #Otherwise someone else sent something and it can be processed or broadcast. 
+                        if readySock != s:
+                            nick, message, proto = sh.read_message(readySock)
+                            print(f'{nick} said {message}')
+
+
             
     except OSError as e:
         print(e)
