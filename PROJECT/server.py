54b1d9e9d2a5292f6da9daa46da0091419e56a4c
MattHill-2025 <matt.hill.1500@gmail.com>
2023-12-12 15:37:10 -0500

    added function descriptions

--- PROJECT/server.py
+++ PROJECT/server.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 # R-E Miller (IT), Matthew Hill (CS), Elliot Swan (CS)
+# R-E Miller (IT), Matthew Hill (CS), Elliot Swan (CS)
+# Author: Matthew Hill, Elliot Swan, R-E Miller
+# Major: Computer Science and IT
+# Creation Date: November 23, 2023
+# Due Date: December 14, 2023
+# Course: csc328
+# Professor Name: Dr. Schwesinger
+# Assignment: Final
+# Filename: server.py
+# Purpose: The purpose of this file is to create a chat server using sockets.
+# Multiple clients(up to 10) are able to connect to the server and chat with one another.
+# All messages are sent to all cleints, and messages are sent when clients leave and the server
+# gracefully shuts down.
 import socket 
 import select
 import sys
@@ -8,20 +21,48 @@ import shared as sh
 import time
 from datetime import datetime
 
+# Function Name: broadcast
+# Description: Send a message from one client or the server to all cleints
+# Parameters: string nick - the name of the person sending the message - input
+#             string message - the message that the client is sending to other clients - input
+#             string proto - what step the client is at(either verify, broadcast, goodbye) - input
+#             array connectionList - all of the connected clients - input
 def broadcast(nick, message, proto, connectionList):
     print(f"{nick} : {message}")
     for connection in connectionList:
         sh.send_message(connection, message, nick, proto)
 
+# Function Name: log_mess
+# Description: logs when clients connect and the messages they send.
+# Parameters: string nick - the name of the person sending the message - input
+#             string message - the message that the client is sending to other clients - input
+#             string proto - what step the client is at(either verify, broadcast, goodbye) - input
+#             FILE Object file_log - the file where the messages are being stored to - input/output
 def log_mess(nick, message, proto, file_log):
     print(f"{nick}: {message}: {datetime.now()}", file = file_log, flush = True )
 
+# Function Name: send_hello
+# Description: Send a message to the client to confirm connection
+# Parameters: File Descriptor s - the socket file descriptor - input
 def send_hello(s):
     world = "HELLO"
     name = "SERVER"
     proto = 'connect'
     sh.send_message(s,world, name, proto)
 
+# Function Name: proto_handle
+# Description: This function handles the different cases that proto can take on.
+# Parameters: string nick - the name of the person sending the message - input
+#             string message - the message that the client is sending to other clients - input
+#             string proto - what step the client is at(either verify, broadcast, goodbye) - input
+#             array connectionList - all of the connected clients - input
+#             File descriptor readysock - all of the sockets ready for reading - input
+#             array strings nickname - the names of all of the taken names ("SERVER" is in by default) - input
+#             FILE Object file_log - the file where message are stored - input/output
+#             dictionary connectioninfo - the dictionary storing each users IP address - input
+#             key connection - the key to the dictionary connection info to obtain the IP address of the specific user - input
+#             string myNick - the nickname of the server "SERVER" which is taken by default - input
+#             array File descriptors myConnectionsSetup - all of the sockets that the clients are connected to - input
 def proto_handle(nick, message, proto, connectionList, readySock, nickname, file_log, connectioninfo, connection, myNick, myConnectionsSetup):
     match proto:
         case "verify":
